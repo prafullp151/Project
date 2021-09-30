456237e406313c04150acace433fbc4f67dd65dc
Prafull More <morepp@mail.uc.edu>
2021-09-30 18:55:49 -0400

    Added empty models

---
+++ RestFoodOrder/RestFoodOrder/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestFoodOrder.Models
+{
+    public class Customer
+    {
+    }
+}
